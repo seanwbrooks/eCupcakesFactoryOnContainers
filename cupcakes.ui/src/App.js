00ee7f6f7857f621733f0c6ca4eac2084de2ddac
srigumm <cnu.gsrinivas@gmail.com>
2019-04-21 11:18:37 -0400

    feat(UI): Add routes for mix and bake processes pages

--- cupcakes.ui/src/App.js
+++ cupcakes.ui/src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Routes from './routes';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1>Cup cakes Factory</h1>
+          <Router>
+            <Routes />
+          </Router> 
+        </div>
       </div>
-    );
-  }
-}
+);
 
 export default App;
---
+++ cupcakes.ui/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Landing from './pages/landing';
+import Mixer from './pages/mixer';
+import Bake from "./pages/bake";
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route path="/mixer" component={Mixer} />
+    <Route path="/bake" component={Bake} />
+  </Switch>
+);
+
+export default Routes;
